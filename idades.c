c1979bdd5a7439abaa05fe67d625472c6dafb210
Temmie099 <124810321+Temmie099@users.noreply.github.com>
2023-06-23 17:14:46 -0300

    Add files via upload

---
+++ idades.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+int main(){
+    int numero_de_pessoas, i;
+    float idades, soma=0, média;
+
+
+    printf("Digite a idade das pessoas");
+    scanf("%d",&numero_de_pessoas);
+
+  for (i=0; < numero_de_pessoas;i++){
+    scanf("%f",& idades);
+    soma+=idades;
+return 0;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
